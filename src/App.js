989e729a9183142acd53ee796c3dac95620ee633
abdulrhmanElsawy <your.email@example.com>
2024-05-23 10:36:12 +0300

    Deployed website

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import { Routes, Route, useLocation } from "react-router-dom";
-import React, { useState, useRef } from "react";
-
-
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 import Landing3 from './components/Landing3';
+import Landing2 from './components/Landing2';
+
 import ScrollToTopButton from './components/ScrollToTopButton';
 
-
-
 function App() {
-
 return (
+    <Router basename="/questions">
     <div className="App">
-            
-        <Landing3 />
+        <Routes>
+        <Route path="/icm" element={<Landing2 />} />
+
+        <Route path="/" element={<Landing3 />} />
+        {/* Add more routes here */}
+        </Routes>
         <ScrollToTopButton />
-
-
-        
     </div>
+    </Router>
 );
 }
 
